884f45d2bbe08426dbbf436b201bbc840ddb6709
madelinemc <madeline.coons@gmail.com>
2020-11-30 17:14:13 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, allow_blank: false
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
